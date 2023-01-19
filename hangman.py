f99e36293932c27995df4513c02c7cf6f27f49f8
Crush-Steelpunch <90606312+Crush-Steelpunch@users.noreply.github.com>
2023-01-19 11:11:33 +0000

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -7,7 +7,8 @@ app = flask.Flask(__name__)
 
 # Database
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@mysql:3306/hangman'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@mysql:3306/hangman'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hang.db'
 db = SQLAlchemy(app)
 
 # Model
